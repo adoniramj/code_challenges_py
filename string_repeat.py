039987d4c352f81539a290840b10de0098653bca
adoniramj <adoniramjvargas@gmail.com>
2020-02-14 18:33:04 -0500

    string_repeat

---
+++ string_repeat.py
@@ -0,0 +1,9 @@
+"""
+Write a function called repeatString which repeats the given String src exactly count times.
+"""
+
+def repeat_str(repeat, string):
+    return string * repeat
+
+
+print(repeat_str(5,'aaaaa '))
